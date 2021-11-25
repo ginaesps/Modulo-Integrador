45cceac43e92b41d1c1dc7c513e7af9cba46ea8f
Javier-Barreto <jbarreto2@ucol.mx>
2021-11-24 18:00:53 -0600

    ASD

--- src/navigators/BottomNavigator/index.tsx
+++ src/navigators/BottomNavigator/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const Tab = createBottomTabNavigator();
 
